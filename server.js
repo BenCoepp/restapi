61c9dbaef4947df24ddae3739595e05dccf19f27
Ben Coepp <bencoepp@gmail.com>
2021-12-08 21:27:57 +0100

    updated restapi

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "dev",
+  password: "root",
+  database: "mysql",
+  port: 3308
+});
+
+con.connect(function(err) {
+  con.query("SELECT * FROM user", function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + JSON.stringify(result));
+  });
+});
 
 const apiKey = '12345'
 
