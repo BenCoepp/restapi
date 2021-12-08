bfd0867d496c1e5b3a1910e670d9443065fc687d
BenCoepp <bencoepp@gmail.com>
2021-12-08 17:22:22 +0100

    init

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const apiKey = 12345
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(200)
+})
+
+app.post('/createuser', (req, res) =>{
+ //TODO needs to be a function
 })
 
 app.listen(port, () => {
