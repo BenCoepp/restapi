ce728b093e91f289c212b86ee4b8d8d9a23f4cb0
Ben Cöppicus <ben.coeppicus@gus-group.com>
2021-12-09 08:13:46 +0100

    updated rest api

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ var con = mysql.createConnection({
   host: "127.0.0.1",
   user: "dev",
   password: "root",
-  database: "mysql",
+  database: "test_db",
   port: 3308
 });
 
 con.connect(function(err) {
-  con.query("SELECT * FROM user", function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + JSON.stringify(result));
-  });
+  if (err) throw err;
+  console.log("Result: " + err);
+  if(err == null){
+    if(checkTablesExist()){
+      
+    }else{
+      
+    }
+  }
 });
+
 
 const apiKey = '12345'
 
@@ -35,6 +41,23 @@ app.post('/createuser', (req, res) =>{
  //create user
  //respond that everything is ok
 })
+
+function checkTablesExist(){
+  con.query("SELECT * FROM information_schema.tables WHERE table_schema = 'test_db';", function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + JSON.stringify(result));
+    if(result == []){
+      return false
+    }else{
+      return true
+    }
+  });
+}
+
+function createTables(){
+  var sqlUSER = "CREATE TABLE USER (USERID INT AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(255), USERNAME VARCHAR(255), PASSWORD VARCHAR(255), CREATINDATE INT, LASTLOGIN INT)"
+  var sqlSESSION = ""
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
