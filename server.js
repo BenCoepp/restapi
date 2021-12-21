34ee97241d0078ac2fcf23a4054cdb527f502a02
Ben Coepp <bencoepp@gmail.com>
2021-12-21 19:37:39 +0100

    updated server

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/', (req, res) => {
   res.send("Hello from the other side")
 })
 
+app.get('/ping', (req, res) => {
+  res.send(200)
+})
+
 app.get('/login', (req, res) => {
   if(apiKey == req.query.apiKey){
     var loginCheckSQL = "SELECT * FROM USER WHERE USERNAME='" + req.query.username +"' AND PASSWORD='"+req.query.password+"'"
@@ -45,7 +49,7 @@ app.get('/login', (req, res) => {
 
 app.post('/createuser', (req, res) =>{
  if(apiKey == req.query.apiKey){
-  var createUser = "INSERT INTO USER (USERRANK,NAME,USERNAME,PASSWORD,CREATINDATE,LASTLOGIN,COUNTRY)VALUES (2 ,'" + req.query.name+"', '" + req.query.username+"', '" + req.query.password+"'," + req.query.creatintime + "," + req.query.lastlogin +",'" + req.query.country+"');"
+  var createUser = "INSERT INTO USER (USERRANK,NAME,USERNAME,PASSWORD,CREATINDATE,LASTLOGIN,COUNTRY,AGE)VALUES (2 ,'" + req.query.name+"', '" + req.query.username+"', '" + req.query.password+"'," + req.query.creatintime + "," + req.query.lastlogin +",'" + req.query.country+"'," +req.query.age + ");"
   con.query(createUser, function (err, result) {
     if (err) throw err;
     if(result !== []){
@@ -59,6 +63,38 @@ app.post('/createuser', (req, res) =>{
 }
 })
 
+app.get('/getuser', (req, res) =>{
+  if(apiKey == req.query.apiKey){
+    var getToken = "SELECT * FROM TOKEN WHERE TOKENSTRING ='" + req.query.token + "'"
+    con.query(getToken, function (err, resultToken) {
+      if (err) throw err;
+      if(resultToken !== []){
+        var getUser = "SELECT * FROM USER WHERE USERNAME = '" + req.query.username + "' AND PASSWORD = '" + req.query.password + "'" 
+        con.query(getUser, function (err, resultUser) {
+          if (err) throw err;
+          if(resultUser !== []){
+            var tokenOBJ = resultToken
+            var userOBJ = resultUser
+            try {
+              if(tokenOBJ[0].userid == userOBJ[0].userid){
+                res.send(JSON.stringify(resultUser))
+              }
+            } catch (error) {
+              res.send(400)
+            }
+          }else{
+            res.send(404)
+          }
+        });
+      }else{
+        res.send(403)
+      }
+    });
+  }else{
+    res.send(403)
+  }
+ })
+
 function checkTablesExist(){
   con.query("SELECT * FROM information_schema.tables WHERE table_schema = 'test_db';", function (err, result) {
     if (err) throw err;
@@ -71,7 +107,7 @@ function checkTablesExist(){
 }
 
 function createTables(){
-  var sqlUSER = "CREATE TABLE USER (USERID INT NOT NULL AUTO_INCREMENT,USERRANK INT NOT NULL ,NAME VARCHAR(255),USERNAME VARCHAR(255),PASSWORD VARCHAR(255),CREATINDATE INT,LASTLOGIN INT,COUNTRY VARCHAR(255),PRIMARY KEY (USERID, USERRANK));"
+  var sqlUSER = "CREATE TABLE USER (USERID INT NOT NULL AUTO_INCREMENT,USERRANK INT NOT NULL ,NAME VARCHAR(255),USERNAME VARCHAR(255),PASSWORD VARCHAR(255),CREATINDATE INT,LASTLOGIN INT,COUNTRY VARCHAR(255),AGE INT,PRIMARY KEY (USERID, USERRANK));"
   var sqlSESSION = "CREATE TABLE SESSION (SESSIONID INT AUTO_INCREMENT,USERID INT,CLICKS MEDIUMTEXT NOT NULL,STARTTIME INT,STOPTIME INT,PLATFORM VARCHAR(255),GOODCLICKS INT,BADCLICKS INT,PRIMARY KEY (SESSIONID, USERID));"
   var sqlTOKEN = "CREATE TABLE TOKEN (TOKENID INT NOT NULL AUTO_INCREMENT,USERID INT NOT NULL , TOKENSTRING VARCHAR(255),PRIMARY KEY (TOKENID, USERID));"
   con.query(sqlUSER, function (err, result) {
@@ -95,24 +131,43 @@ app.get('/getsession', (req, res) =>{
 })
 
 app.post('/createsession', (req, res) =>{
+  console.log(JSON.stringify(req.query.userid))
   if(apiKey == req.query.apiKey){
-    var token = ""
-    con.query("SELECT TOKENSTRING FROM TOKEN WHERE USERID = "+ req.query.userid, function (err, result) {
+    var getToken = "SELECT * FROM TOKEN WHERE TOKENSTRING ='" + req.query.token + "'"
+    con.query(getToken, function (err, resultToken) {
       if (err) throw err;
-      if(result == ""){
-        res.send(403)
+      if(resultToken !== []){
+        var getUser = "SELECT * FROM USER WHERE USERID = " + req.query.userid 
+        con.query(getUser, function (err, resultUser) {
+          if (err) throw err;
+          if(resultUser !== []){
+            var tokenOBJ = resultToken
+            var userOBJ = resultUser
+            try {
+              if(tokenOBJ[0].userid == userOBJ[0].userid){
+                var createSESSION = "INSERT INTO SESSION (USERID,CLICKS,STARTTIME,STOPTIME,PLATFORM,GOODCLICKS,BADCLICKS) VALUES ("+req.query.userid + " ,'" + req.query.clicks+"', " + req.query.starttime+", " + req.query.stoptime+",'" + req.query.platform + "'," + req.query.goodclicks +"," + req.query.badclicks+");"
+                con.query(createSESSION, function (err, result) {
+                  if (err) throw err;
+                  if(result !== []){
+                    res.send(200)
+                  }else{
+                    res.send(403)
+                  }
+                });
+              }
+            } catch (error) {
+              res.send(400)
+            }
+          }else{
+            res.send(404)
+          }
+        });
       }else{
-        if(result == req.query.token){
-          con.query("INSERT INTO SESSION () VALUES ()", function (err, result) {
-            if (err) throw err;
-            res.send(200)
-          });
-        }
+        res.send(403)
       }
     });
-    if(token == req.query.token){
-      
-    }
+  }else{
+    res.send(403)
   }
 })
 
