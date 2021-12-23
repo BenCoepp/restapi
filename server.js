1beccd3fcc332f3615f27325c967a1e349af26aa
Ben Cöppicus <ben.coeppicus@gus-group.com>
2021-12-23 08:13:31 +0100

    updated server

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ con.connect(function(err) {
   }
 });
 
+app.use(function (req, res, next){
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  // Pass to next layer of middleware
+  next();
+});
+
 
 const apiKey = '12345'
 
